5326ebd2c37bf574f49fd24e0a8c4be8b5bb7d8d
doankhiem <khiembi161219@gmail.com>
2024-01-25 21:59:51 +0700

    install vitejs and update vue route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import admin from "./admin";
-const routes = [...admin];
+import InvoiceIndex from '../components/invoice/index.vue';
+import NotFound from '../components/NotFound.vue';
+const routeInvoice = [
+    {
+        path: '/',
+        component: InvoiceIndex
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: NotFound,
+    }
+]
+const routes = [...admin, ...routeInvoice];
 
 const router = createRouter({
     history: createWebHistory(),
