ec27d4a7cd1fa198ecbaac5eeddffea41b001ee3
doankhiem <khiembi161219@gmail.com>
2024-01-30 17:49:19 +0700

    Create Invoice - add form data and template.

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,16 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import admin from "./admin";
 import InvoiceIndex from '../components/invoice/index.vue';
 import NotFound from '../components/NotFound.vue';
+import invoiceNew from '../components/invoice/new.vue'
+
 const routeInvoice = [
     {
         path: '/',
         component: InvoiceIndex
     },
     {
+        path: '/invoice/new',
+        component: invoiceNew
+    },
+    {
         path: '/:pathMatch(.*)*',
         component: NotFound,
     }
+
 ]
 const routes = [...admin, ...routeInvoice];
 
